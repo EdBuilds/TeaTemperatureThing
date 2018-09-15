834ad938d199b970bd72e8143e8d68573c660103
EdBuilds <edbuildsoriginal@gmail.com>
2018-09-15 12:00:31 +0200

    ADC init, not tested

---
+++ inc/Thermometer_driver.hpp
@@ -0,0 +1,16 @@
+/*
+ * ADC_driver.hpp
+ *
+ *  Created on: Aug 10, 2018
+ *      Author: tamas
+ */
+
+#ifndef THERMOMETER_DRIVER_HPP_
+#define THERMOMETER_DRIVER_HPP_
+class Thermometer{
+	 Thermometer();
+};
+
+
+
+#endif /* THERMOMETER_DRIVER_HPP_ */
