aed44d43e96edc625ac77346abad11ce0a37b55d
EdBuilds <edbuildsoriginal@gmail.com>
2018-09-23 22:42:03 +0200

    Working low power mode

--- src/main.cpp
+++ src/main.cpp
@@ -14,60 +14,80 @@
 #include "display_driver.hpp"
 #include "Buzzer_driver.hpp"
 void SystemClock_Config(void);
+void SystemPower_Config(void);
 void _Error_Handler(char *file, int line);
 int main(void)
 {
+	HAL_Init();
 SystemClock_Config();
-Buzzer buzzer;
-buzzer.init_timer();
-buzzer.setFrequency(16000);
-buzzer.start();
+SystemPower_Config();
+//Buzzer buzzer;
+//buzzer.init_timer();
+//buzzer.setFrequency(16000);
+//buzzer.start();
+HAL_PWR_EnableWakeUpPin(PWR_WAKEUP_PIN1);
+HAL_PWR_EnableWakeUpPin(PWR_WAKEUP_PIN2);
+HAL_PWR_EnableWakeUpPin(PWR_WAKEUP_PIN3);
+
+__HAL_PWR_CLEAR_FLAG(PWR_FLAG_WU);
+
+ /* Disable GPIOs clock */
+ __HAL_RCC_GPIOA_CLK_DISABLE();
+ __HAL_RCC_GPIOB_CLK_DISABLE();
+ /* Enter Stop Mode */
+ HAL_SuspendTick();
+ HAL_RCC_DeInit();
+ HAL_DeInit();
+//HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
+HAL_PWR_EnterSTANDBYMode();
 	for(;;);
 }
 //Cleanup this code dude
- void SystemClock_Config(void)
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  /* Enable MSI Oscillator */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.MSICalibrationValue=0x00;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+
+  /* Select MSI as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the device is
+     clocked below the maximum system frequency, to update the voltage scaling value
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /* Disable Power Control clock */
+  __HAL_RCC_PWR_CLK_DISABLE();
+
+}
+
+ void SystemPower_Config(void)
  {
-
-   RCC_OscInitTypeDef RCC_OscInitStruct;
-   RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
-     /**Configure the main internal regulator output voltage
-     */
-   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-     /**Initializes the CPU, AHB and APB busses clocks
-     */
-   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-   RCC_OscInitStruct.HSICalibrationValue = 16;
-   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_4;
-   RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_4;
-   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-   {
-   //  _Error_Handler(__FILE__, __LINE__);
-   }
-
-     /*Initializes the CPU, AHB and APB busses clocks*/
-   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-   {
-//     _Error_Handler(__FILE__, __LINE__);
-   }
-
-     /**Configure the Systick interrupt time
-     */
-   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-     /**Configure the Systick
-     */
-   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-   /* SysTick_IRQn interrupt configuration */
-   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+   __HAL_RCC_PWR_CLK_ENABLE();
+   HAL_PWREx_EnableUltraLowPower();
+   HAL_PWREx_EnableLowPowerRunMode();
  }
