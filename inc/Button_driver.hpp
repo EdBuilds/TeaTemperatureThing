91b279d87a44a5c1dbcaa5d4bc47a966d9e2721d
EdBuilds <edbuildsoriginal@gmail.com>
2018-10-02 22:24:52 +0200

    Add source files

---
+++ inc/Button_driver.hpp
@@ -0,0 +1,18 @@
+/*
+ * Button_driver.hpp
+ *
+ *  Created on: Oct 2, 2018
+ *      Author: tamas
+ */
+
+#ifndef BUTTON_DRIVER_HPP_
+#define BUTTON_DRIVER_HPP_
+
+class Button{
+void Init();
+};
+
+
+
+
+#endif /* BUTTON_DRIVER_HPP_ */
---
+++ src/Button_driver.cpp
@@ -0,0 +1,12 @@
+/*
+ * Button_driver.cpp
+ *
+ *  Created on: Oct 2, 2018
+ *      Author: tamas
+ */
+#include "Button_driver.hpp"
+
+void Button::Init(){
+
+}
+
